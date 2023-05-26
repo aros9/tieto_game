6246cb0d6bb1d3f2d226ebb31677e7614f8fbb91
Kamil <kkowalczyk848@gmail.com>
2023-05-26 13:30:58 +0200

    Added attack table

--- Player/inc/Action.hpp
+++ Player/inc/Action.hpp
@@ -1,6 +1,20 @@
 #pragma once
 #include "Status.hpp"
 
+constexpr int UNIT_COUNT = 8;
+
+const int AttackTable[UNIT_COUNT - 1][UNIT_COUNT] = 
+{
+  // K   S   A   P   C   R   W   B
+    {35, 35, 35, 35, 35, 50, 35, 35}, // Knight
+    {30, 30, 30, 20, 20, 30, 30, 30}, // Swordsman
+    {15, 15, 15, 15, 10, 10, 15, 15}, // Archer
+    {35, 15, 15, 15, 15, 10, 15, 10}, // Pikeman
+    {40, 40, 40, 40, 40, 40, 40, 50}, // Catapult
+    {10, 10, 10, 10, 10, 10, 10, 50}, // Ram
+    {5,  5,  5,  5,  5,  5,  5,  1} // Worker
+};
+
 class Action
 {
     public:
